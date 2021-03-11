4db1fd17cb09497903f778482d21c627e3c0d9f5
“Oluwaseyi3” <iamoluwaseyiolasupo@gmail.com>
2021-03-11 12:00:10 +0000

    last

--- back-end/client/src/CheckTwo.js
+++ back-end/client/src/CheckTwo.js
@@ -1,59 +1,59 @@
-import React, {useReducer, useEffect, useContext} from "react";
+// import React, {useReducer, useEffect, useContext} from "react";
 
-import {Redirect} from "react-router-dom";
-import axios  from "axios"
-import {UserContext} from "./context/UserContext.js"
-import UseLocalStorageState from "./useLocalStorageState.js"
-import { setAuthentication } from "./authStorage.js";
+// import {Redirect} from "react-router-dom";
+// import axios  from "axios"
+// import {UserContext} from "./context/UserContext.js"
+// import UseLocalStorageState from "./useLocalStorageState.js"
+// import { setAuthentication } from "./authStorage.js";
 
-export const initialState = {
-    error: "",
-    image: {}
-}
+// export const initialState = {
+//     error: "",
+//     image: {}
+// }
 
 
 
-export const reducer = (state, action) => {
-    switch(action.type){
-      case "FETCH_SUCCESS":
-          return {
-              image: action.payload
-          }
+// export const reducer = (state, action) => {
+//     switch(action.type){
+//       case "FETCH_SUCCESS":
+//           return {
+//               image: action.payload
+//           }
 
-        case "FETCH_ERROR":
-            return {
-                image: {},
-                error: "Something went wrong"
-            }
+//         case "FETCH_ERROR":
+//             return {
+//                 image: {},
+//                 error: "Something went wrong"
+//             }
         
-        default:
-            return state
-    }
-}
+//         default:
+//             return state
+//     }
+// }
 
-export default function CheckTwo(props) {
-    const { userData, setUserData} = useContext(UserContext);
-    // const [userInfo, setUserInfo] = UseLocalStorageState("userId", userData)
+// export default function CheckTwo(props) {
+//     const { userData, setUserData} = useContext(UserContext);
+//     // const [userInfo, setUserInfo] = UseLocalStorageState("userId", userData)
 
-    const [state, dispatch] = useReducer(reducer, initialState)
+//     const [state, dispatch] = useReducer(reducer, initialState)
   
    
     
-useEffect(()=> {
+// useEffect(()=> {
 
 
         
-        try {
+//         try {
   
          
              
-            async function GetData(){
+//             async function GetData(){
               
      
              
-              const loggedId = localStorage.getItem("userId")
-              const foundUser = JSON.parse(loggedId)
-                const response = await axios.get(`http://localhost:5000/users/getLatest/${foundUser}`)
+//               const loggedId = localStorage.getItem("userId")
+//               const foundUser = JSON.parse(loggedId)
+//                 const response = await axios.get(`http://localhost:5000/users/getLatest/${foundUser}`)
               
                 
                
@@ -61,23 +61,23 @@ useEffect(()=> {
                 
                 
          
-                dispatch({type: "FETCH_SUCCESS",payload:response.data} )
+//                 dispatch({type: "FETCH_SUCCESS",payload:response.data} )
              
                
-            }
+//             }
           
-            GetData()
-        } catch (error) {
-            dispatch({type: "FETCH_ERROR"} )
-        }
+//             GetData()
+//         } catch (error) {
+//             dispatch({type: "FETCH_ERROR"} )
+//         }
 
      
  
-    }, []) 
+//     }, []) 
 
-    return (
-     <div>
-       <img src={state.image.imageUrl}  />
-     </div>
-    )
-}
+//     return (
+//      <div>
+//        <img src={state.image.imageUrl}  />
+//      </div>
+//     )
+// }
