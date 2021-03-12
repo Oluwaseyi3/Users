c23d8767ad91842d090b5e1728e5096633edb1b0
“Oluwaseyi3” <iamoluwaseyiolasupo@gmail.com>
2021-03-12 06:56:07 +0000

    made changes

--- back-end/index.js
+++ back-end/index.js
@@ -33,7 +33,7 @@ app.use("/users", require("./routes/userRouter"))
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "8080";
 
     // ... other app.use middleware 
 app.use(express.static(path.join(__dirname, "client", "build")))
